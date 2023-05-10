fe376d24e31f5b49e66b955c00b8e86d932ac5b8
Harrison Ryan <hwryan12@gmail.com>
2023-05-10 13:18:35 -0500

    feature: List input

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const ulEl = document.getElementById('ul-el');
 
 inputBtn.addEventListener('click', function () {
   myLeads.push(inputEl.value);
+  inputEl.value = "";
   renderLeads();
 })
 
 function renderLeads() {
-  let listItems = "";
+  let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+      listItems += `
+          <li>
+              <a target='_blank' href='${myLeads[i]}'>
+                  ${myLeads[i]}
+              </a>
+          </li>
+      `
   }
-  ulEl.innerHTML = listItems;
+  ulEl.innerHTML = listItems  
 }
